db793a4682c2eaedf6115109fc2b6a1d4380be35
watsabe <julius.tva@gmail.com>
2016-05-22 17:41:15 +0300

    doctestai v0.5

--- lokomotyvas.py
+++ lokomotyvas.py
@@ -1,6 +1,20 @@
+import doctest
 class Lokomotyvas:
 
     def __init__(self, name, mase, tempGalia):
+        """test init
+	>>> a = Lokomotyvas("lokomotyvas",10,1000)
+        >>> print(a)
+        Lokomotyvas = lokomotyvas  Mase = 10, tempiamoji galia = 1000
+        
+        >>> a.getLokName()
+        'lokomotyvas'
+        >>> a.getLokMase()
+        10
+        >>> a.getLokGalia()
+        1000
+	"""
+
         self.name = name
         self.mase = mase
         self.tempGalia = tempGalia
@@ -21,4 +35,8 @@ class Lokomotyvas:
         return "%s" % (self.name)
 
     def __str__(self):
-        return "Lokomotyvas = %s  Mase = %s, tempiamoji galia = %s " % (self.name, self.mase, self.tempGalia)
+        return "Lokomotyvas = %s  Mase = %s, tempiamoji galia = %s" % (self.name, self.mase, self.tempGalia)
+       
+
+if __name__ == "__main__":
+	doctest.testmod()
